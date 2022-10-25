e821dc48d8cb4c3f9fc88acf9ccd9c14c9386daa
JerickoDeGuzman <qjmdeguzman01@tip.edu.ph>
2022-10-25 14:56:38 +0000

    latestCommitjericko-branch

--- flask-web-app.py
+++ flask-web-app.py
@@ -1,12 +1,31 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+from mapquestBackend import calculateDirection
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def main():
-    return render_template("index.html")
+    if request.method == "POST":
+        orig = request.form['start']
+        dest = request.form['dest']
+
+        tripDuration, totalKm, totalFuelLtr, directionList, json_status = calculateDirection(orig, dest)
+        if json_status == 0:
+            return render_template("index.html", tripDuration=tripDuration, totalKm=totalKm, totalFuelLtr=totalFuelLtr, directionList=directionList, request=request)
+        elif json_status == 402:
+            message = f"Status Code: {json_status}; Invalid user inputs for one or both locations."
+            return render_template("baseError.html", message=message, jsonStatus=json_status)
+        elif json_status == 611:
+            message = f"Status Code: {json_status}; Missing an entry for one or both locations."
+            return render_template("baseError.html", message=message, jsonStatus=json_status)
+        else:
+            message = f"For Status Code: {json_status}; Refer to: https://developer.mapquest.com/documentation/directions-api/status-codes"
+            return render_template("baseError.html", message=message, jsonStatus=json_status)
+      
+    else:
+        return render_template("index.html")
 
 
 

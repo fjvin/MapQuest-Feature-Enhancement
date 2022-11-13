bd00a0384904cff418c30713355177c181ee5e9d
JerickoDeGuzman <qjmdeguzman01@tip.edu.ph>
2022-11-13 05:27:49 +0000

    withStaticAndTest

--- mapquest.sh
+++ mapquest.sh
@@ -7,6 +7,7 @@ mkdir tempdir/referenceFiles
 
 cp flask-web-app.py tempdir/.
 cp mapquestBackend.py tempdir/.
+cp testMapquest.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 cp -r referenceFiles/* tempdir/referenceFiles/.
@@ -16,8 +17,11 @@ echo "RUN pip install flask" >> tempdir/Dockerfile
 echo "RUN pip install requests" >> tempdir/Dockerfile
 echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
 echo "COPY  ./referenceFiles /home/myapp/referenceFiles/" >> tempdir/Dockerfile
+echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY  flask-web-app.py /home/myapp/" >> tempdir/Dockerfile
 echo "COPY  mapquestBackend.py /home/myapp/" >> tempdir/Dockerfile
+echo "COPY  testMapquest.py /home/myapp/" >> tempdir/Dockerfile
+
 echo "EXPOSE 5050" >> tempdir/Dockerfile
 echo "CMD python3 /home/myapp/flask-web-app.py" >> tempdir/Dockerfile
 

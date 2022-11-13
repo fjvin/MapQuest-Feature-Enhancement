a6527283a77069eb789251b49f22bdeb2db6866c
JerickoDeGuzman <qjmdeguzman01@tip.edu.ph>
2022-11-13 00:57:01 +0000

    port6060fix

--- mapquest.sh
+++ mapquest.sh
@@ -10,15 +10,16 @@ cp -r templates/* tempdir/templates/.
 cp -r referenceFiles/* tempdir/referenceFiles/.
 
 echo "FROM python" >> tempdir/Dockerfile
-echo "RUN pip install install flask" >> tempdir/Dockerfile
+echo "RUN pip install flask" >> tempdir/Dockerfile
+echo "RUN pip install requests" >> tempdir/Dockerfile
 echo "COPY ./templates /home/myapp/templates/" >> tempdir/Dockerfile
 echo "COPY ./referenceFiles /home/myapp/referenceFiles/" >> tempdir/Dockerfile
 echo "COPY flask-web-app.py /home/myapp/" >> tempdir/Dockerfile
 echo "COPY mapquestBackend.py /home/myapp/" >> tempdir/Dockerfile
-echo "EXPOSE 8080" >> tempdir/Dockerfile
-echo "CMD python3 /home/myapp/flask-web-app.py" >> tempdir/Dockerfile
+echo "EXPOSE 6060" >> tempdir/Dockerfile
+echo "CMD python /home/myapp/flask-web-app.py" >> tempdir/Dockerfile
 
 cd tempdir
 docker build -t mapquestapp .
-docker run -t -d -p 8080:8080 --name mapquestapprunning mapquestapp
+docker run -t -d -p 5050:5050 --name mapquestapprunning mapquestapp
 docker ps -a
